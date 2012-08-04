a92685d8c3b28a7e64c30797c595b9a66851dad6
Alex Landau <bal2ag@virginia.edu>
2012-08-03 21:05:16 -0400

    added keys for Parse app, some notes about handling data

--- config.py
+++ config.py
@@ -5,4 +5,10 @@
 CALENDAR_ID='bal2ag@virginia.edu'
 
 #The google calendar API key.
-API_KEY='AIzaSyAdvIBlSWMdn-Ed4c_BmQUI86-GuEFrVl0'
+GOOGLE_API_KEY='AIzaSyAdvIBlSWMdn-Ed4c_BmQUI86-GuEFrVl0'
+
+#The application ID for Parse.
+PARSE_APP_ID='dF4lf8kvjIWL3PqE8ANdS5ancdatrrlYMw0Dm2nM'
+
+#The REST API key for Parse.
+PARSE_REST_KEY = '1sRa9Ok1c5UOLKDB8glwq95KEk6mN0EITvAkaDQz'
--- scraper.py
+++ scraper.py
@@ -3,8 +3,11 @@
 #Python dictionaries.
 
 from config import CALENDAR_ID
-from config import API_KEY
+from config import GOOGLE_API_KEY
+from config import PARSE_APP_ID
+from config import PARSE_REST_KEY
 import json
+import redis
 import httplib
 import urllib2
 import datetime
@@ -14,7 +17,7 @@ import datetime
 GET_EVENTS='https://www.googleapis.com/calendar/v3/'\
                 'calendars/{cid}/events/?key={key}'\
 		'&singleEvents=true&orderBy=starttime'\
-		.format(cid=CALENDAR_ID,key=API_KEY)
+		.format(cid=CALENDAR_ID,key=GOOGLE_API_KEY)
 
 request = urllib2.Request(GET_EVENTS)
 
@@ -26,5 +29,17 @@ except urllib2.HTTPError, e:
     print e
     exit(1)
 
-#TODO: Parse the events dictionary and create a more manageable dictionary for
-#easy export to Parse.
+events_to_push = []
+
+for one_event in events_list_dict['items']:
+    event_to_push = {}
+    print "\tID: " + one_event[u'id']
+    print "\tTITLE: " + one_event[u'summary']
+    if u'location' in one_event:
+        print "\tLOC: " + one_event[u'location']
+    #TODO: In this loop, for every event, store event data as a dictionary. Add
+    #these dictionaries to a list. Then, process this list, pushing each event to
+    #Parse and storing a mapping from the event id to the Parse object ID on a
+    #local redis database. [Can determine whether or not to push to Parse based
+    #on whether the event ID is already stored in redis]
+   
