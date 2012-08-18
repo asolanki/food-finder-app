4cd17fb4d24f6ab844b7408a1fc52c872a555f71
Alex Landau <bal2ag@virginia.edu>
2012-08-18 01:53:10 -0400

    lots of major backend work, code tested a bit

--- process_data.py
+++ process_data.py
@@ -20,16 +20,19 @@ redis_logger = logging.getLogger('redis')
 redis_handler = logging.FileHandler('logs/redis.log')
 redis_handler.setFormatter(log_format)
 redis_logger.addHandler(redis_handler)
+redis_logger.setLevel(logging.INFO)
 
 parse_logger = logging.getLogger('parse')
 parse_handler = logging.FileHandler('logs/parse.log')
 parse_handler.setFormatter(log_format)
 parse_logger.addHandler(parse_handler)
+parse_logger.setLevel(logging.INFO)
 
 stdio_logger = logging.getLogger('stdio')
 stdio_handler = logging.FileHandler('logs/stdio.log')
 stdio_handler.setFormatter(log_format)
 stdio_logger.addHandler(stdio_handler)
+stdio_logger.setLevel(logging.INFO)
 
 #String constant for google calendar API request, based on API key and Calendar
 #ID
@@ -59,9 +62,8 @@ def handleLocation(loc_in):
         response = urllib2.urlopen(request)
         location_dict = json.loads(response.read())['results'][0][u'geometry']\
                                                                  [u'location']
-    except urllib2.HTTPError, e:
-        print e
-        exit(1)
+    except urllib2.HTTPError as e:
+        return { 'ERROR' : str(e), 'API_CALL' : loc_in }
 
     return_dict = {}
     return_dict['latitude'] = location_dict[u'lat']
@@ -83,8 +85,15 @@ def get_food_event(one_event):
     	'most_recent_time' : one_event[u'updated']
     }
     coords = handleLocation(food_event['location'])
-    food_event['latitude'] = coords['latitude']
-    food_event['longitude'] = coords['longitude']
+    if 'ERROR' in coords:
+        stdio_logger.warn('Google places API call failed on'\
+        ' {0}'.format(coords['API_CALL']))
+    else:
+        food_event['coordinates'] = {
+                '__type' : 'GeoPoint',
+                'latitude' : coords['latitude'],
+                'longitude' : coords['longitude'],
+        }
     return food_event
 
 #Given a food_event dict and a redis db instance, dump the fields in the
@@ -92,25 +101,40 @@ def get_food_event(one_event):
 # @param redis_db A local redis database instance.
 # @param food_event A formatted food_event dictionary
 def update_redis(redis_db, food_event):
+    redis_logger.info('Dumping event {0} into'\
+                      ' redis'.format(food_event['event_id']))
+#TODO: Add error checking
     for food_event_attr in food_event.keys():
         if food_event_attr != 'event_id':
             redis_db.set(food_event['event_id']+'-'+food_event_attr,\
                     food_event[food_event_attr])
 
+    redis_logger.info('Event successfully dumped to redis.')
+
+
 #Make a request to the Parse cloud. Default is a POST, but could also be a PUT
 #(for updating)
 # @param food_obj A formatted food_event dictionary
 # @param req The type of Parse request to make. Default is POST.
-def parse_req(food_obj, req='POST'):
+def parse_req(redis_db, food_obj, req='POST'):
+#TODO: Add error checking
+    parse_logger.info('Making parse {0} call on event'\
+    ' {1}'.format(req,food_obj['event_id']))
     connection = httplib.HTTPSConnection('api.parse.com', 443)
     connection.connect()
-    connection.request(req, '/1/classes/FoodObject', json.dumps(food_obj),\
+    if req=='POST':
+        endpoint = '/1/classes/FoodEvent'
+    else:
+        parseid = redis_db.get(food_obj['event_id'])
+        endpoint = '/1/classes/FoodEvent/{0}'.format(parseid)
+    connection.request(req, endpoint, json.dumps(food_obj),\
                        {                        
                            "X-Parse-Application-Id": PARSE_APP_ID,
                            "X-Parse-REST-API-Key": PARSE_REST_KEY,
                            "Content-Type": "application/json"
                        })
     result = json.loads(connection.getresponse().read())
+    parse_logger.info('Parse {0} call successful'.format(req))
     return result
 
 
@@ -127,23 +151,23 @@ def api_req(url):
         response = urllib2.urlopen(request)
         return json.loads(response.read())
     except e:
-        return { "ERROR" : e }
+        return { 'ERROR' : e, 'API_CALL' : url }
 
 #Main script. Job is to retrieve data from google calendar, extract relevant
 #fields, save the data in the local redis instance currently running, and push
 #the data up to the Parse cloud. Should be able to handle both new events and
 #updated/changed events (based on what is already saved in the local redis DB)
 def main():
-    redis_logger.info('Opening redis db at {0} on port {1} at database {2}')\
-                     .format(REDIS['host'],REDIS['port'],REDIS['db'])
+    redis_logger.info('Opening redis db at {0} on port {1} at database {2}'\
+                     .format(REDIS['host'],REDIS['port'],REDIS['db']))
     redis_db = redis.StrictRedis(\
             host=REDIS['host'],\
             port=REDIS['port'],\
             db=REDIS['db'])
     try:
         redis_db.ping()
-    except e:
-        redis_logger.error(err)
+    except redis.exceptions.ConnectionError as err:
+        redis_logger.error(str(err))
         exit(1) 
 
     redis_logger.info('Successfully opened redis db')
@@ -151,8 +175,9 @@ def main():
     events_list_dict = api_req(GET_EVENTS)
 
     #Did we hit an error on the Google Calendar call? If so, log it and exit.
-    if events_list_dict['ERROR']:
-        print events_list_dict['ERROR']
+    if 'ERROR' in events_list_dict:
+        stdio_logger.error('Error on google calendar (api call: {0})'\
+                            .format(events_list_dict['API_CALL']))
         exit (1)
 
     #Main loop for processing events from Google calendar.
@@ -178,20 +203,29 @@ def main():
             if delta != zerotime:
                 # Could be slightly more efficient if we only updated location when
                 # it actually changes, but this should be fine for now
+                stdio_logger.info('Updating event {0}'.format(one_event[u'id']))
                 food_event = get_food_event(one_event)
                 update_redis(redis_db, food_event)
-                parse_req(food_event, 'PUT')
+                parse_req(redis_db, food_event, 'PUT')
+            else:
+                stdio_logger.info('Skipping event {0}, no update needed.'\
+                                  .format(one_event[u'id']))
 
         #Dealing with a create (new event)
         else:
+            stdio_logger.info('Creating event {0}'.format(one_event[u'id']))
             food_event = get_food_event(one_event)
             update_redis(redis_db, food_event)
-            reply = parse_req(food_event)
+            reply = parse_req(redis_db, food_event)
             redis_db.set(food_event['event_id'], reply['objectId'])
+
+    #TODO: Check for deleted events by getting every event ID from redis and
+    #ensuring that it is contained in the events ids retrieved from google
+    #calendar.
 
     #After all our work is done, save the Redis DB to disk.
     redis_db.save()
 
 
 #Start it up...
-#main() 
+main() 
